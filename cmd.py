e71f1489134cef3ef0eeb6891a240e3de5a112f6
evi1haxor <architdwivedi.off@gmail.com>
2018-11-28 19:25:33 +0530

    Written push script once and for all #2

--- cmd.py
+++ cmd.py
@@ -1,8 +1,8 @@
 import subprocess
 import argparse
 p = argparse.ArgumentParser()
-p.add_argument("stg", help = "Stage What??")
-p.add_argument("commit_message", help = "CommitMessage")
+p.add_argument("stg", help = "specs for stage")
+p.add_argument("commit_message", help = "CommitMessage <message should be in triple dual-quotes.>")
 a = p.parse_args()
 what = a.stg
 CommitMessage = a.commit_message
